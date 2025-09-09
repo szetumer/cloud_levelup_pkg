3e68cfef433cb41a9d20a7c1f75b564de5feb8a0
Samuel Zetumer <samuel.zetumer@va.gov>
2025-09-09 10:12:32 -0500

    added cost reduction of cluster to level 4

--- tests/4_databricks1_tests.py
+++ tests/4_databricks1_tests.py
@@ -28,7 +28,33 @@ def test_databricks_cluster_id_in_proper_place():
     j : list[dict] = GetAzure._json("databricks", "clusters", "list", "--output", "json")
     print(j)
     assert(cluster_id in [d["cluster_id"] for d in j])
+
+class TestClusterIsNotExpensive:
+
+    @pytest.fixture(scope="class")
+    def cluster_attrs(self) -> dict:
+        configs : Config = Config(databricks_config_filepath)
+        cluster_id : str = configs.configs["cluster1_id"]
+        j : list[dict] = GetAzure._json("databricks", "clusters", "list", "--output", "json")
+        cluster_attrs : dict = [d for d in j if d["cluster_id"] == cluster_id][0]
+        return cluster_attrs
     
+    def test_cluster_has_autotermination_30min_or_under(self, cluster_attrs : dict):
+        print(cluster_attrs)
+        assert(int(cluster_attrs["autotermination_minutes"]) <= 30)
+
+    def test_cluster_is_single_node(self, cluster_attrs : dict):
+        print(cluster_attrs)
+        assert(cluster_attrs["is_single_node"])
+
+    def test_cluster_is_d2_or_b_series(self, cluster_attrs : dict):
+        print(cluster_attrs)
+        assert("_d2" in str(cluster_attrs["node_type_id"]).lower() or "_b" in str(cluster_attrs["node_type_id"]).lower())
+
+    def test_cluster_is_terminated(self, cluster_attrs : dict):
+        print(cluster_attrs)
+        assert(cluster_attrs["state"] == "TERMINATED")
+
 def test_databricks_workspace_path_understand():
     configs : Config = Config(databricks_config_filepath)
     workspace_path : str = configs.configs["workspace_path"]
@@ -39,6 +65,9 @@ def test_databricks_workspace_path_understand():
 def test_databricks_filesystem_contains_csv_file():
     configs = Config(databricks_config_filepath)
     dbfs_folderpath : str = configs.configs["dbfs_folderpath"]
+    if dbfs_folderpath is None:
+        pytest.fail(f"you need to enter a DBFS file path into the {databricks_config_filepath} with a csv file inside it.")
     j : list[dict] = GetAzure._json("databricks", "fs", "ls", dbfs_folderpath, "--output", "json")
     print(j)
-    assert(any([".csv" in d["name"] for d in j]))
+    if not any([".csv" in d["name"] for d in j]):
+        pytest.fail(f"you need to enter a DBFS file path into the {databricks_config_filepath} with a csv file inside it.")
